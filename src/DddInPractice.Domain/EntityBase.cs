937b0c2cc5fe1269a2aed2a0034c731cd885a76f
Moses <mmosa@mosesofegypt.net>
2022-03-06 22:59:27 +1100

    Module 02 complete

---
+++ src/DddInPractice.Domain/EntityBase.cs
@@ -0,0 +1,47 @@
+﻿namespace DddInPractice.Domain
+{
+    public abstract class EntityBase
+    {
+        public long Id { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as EntityBase;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == 0 || other.Id == 0)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(EntityBase a, EntityBase b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(EntityBase a, EntityBase b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
+    }
+}
---
+++ src/DddInPractice.Domain/ValueObjectBase.cs
@@ -0,0 +1,41 @@
+﻿namespace DddInPractice.Domain
+{
+    public abstract class ValueObjectBase<T>
+        where T : ValueObjectBase<T>
+    {
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(valueObject, null))
+                return false;
+
+            return EqualsCore(valueObject);
+        }
+
+        protected abstract bool EqualsCore(T other);
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        protected abstract int GetHashCodeCore();
+
+        public static bool operator ==(ValueObjectBase<T> a, ValueObjectBase<T> b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObjectBase<T> a, ValueObjectBase<T> b)
+        {
+            return !(a == b);
+        }
+    }
+}
